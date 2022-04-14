cf1a0dfbf9d3332a8ba870f446962774509ad59b
Zaragoza2123 <zaragoza.coyote618@gmail.com>
2022-04-14 15:58:29 -0500

    swca

---
+++ src/main/java/com/codingdojo/book/JavaBooksApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBooksApplication.class, args);
+	}
+
+}
